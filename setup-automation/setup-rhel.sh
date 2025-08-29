b64f4f04a732ef4a54c5ff4c82001f42f514cd47
Jscar Hawk <57020043+jscar-hawk@users.noreply.github.com>
2025-08-29 10:46:08 -0700

    Update setup-rhel.sh

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-while [ ! -f /opt/instruqt/bootstrap/host-bootstrap-completed ]
-do
-   echo "Waiting for Instruqt to finish booting the VM"
-   sleep 1
-done
-
-subscription-manager clean
-subscription-manager register --activationkey=${ACTIVATION_KEY} --org=12451665 --force
-
-echo "Adding wheel" > /root/post-run.log
-usermod -aG wheel rhel
-
-echo "setting password" >> /root/post-run.log
-echo redhat | passwd --stdin rhel
-
-echo "adding cockpit" >> /root/post-run.log
-dnf -y install cockpit cockpit-packagekit
-
-echo "starting cockpit" >> /root/post-run.log
-systemctl start cockpit
 
 echo "exclude=kernel*" >> /etc/yum.conf
 
